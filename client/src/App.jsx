e6cfe77d3d073dffe8ea8272afc8975f70c65aa7
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-09-20 01:37:25 +0530

    created number of directories for project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">App</div>
   )
 }
 
---
+++ client/src/utils/index.js
@@ -0,0 +1,22 @@
+export const daysLeft = (deadLine) => {
+    const difference = new Date(deadLine).getTime() - Date.now();
+    const remainingDays = difference / (1000 * 3600 * 24);
+
+    return remainingDays.toFixed(0);
+};
+
+export const calculateBarPercentage = (goal, raisedAmount) => {
+    const percentage = Math.round((raisedAmount * 100) / goal);
+
+    return percentage;
+};
+
+export const checkIfImage = (url, callback) => {
+    const img = new Image();
+    img.src = url;
+
+    if (img.complete) callback(true);
+
+    img.onload = () => callback(true);
+    img.onerror = () => callback(true);
+};
