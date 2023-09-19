bc60984872d15a9ed3339bc5d3d2ca0b88ae78a1
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-09-20 02:28:26 +0530

    create two components Sidebar & Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
+
 const App = () => {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
