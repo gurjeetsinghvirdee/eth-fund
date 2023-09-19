d40132fa9ceb8f10354db0e50e3f6e149aecb8de
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-09-20 00:56:41 +0530

    js changed to jsx, installed & updated some libraries

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { ReactDOM } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdWebProvider } from '@thirdweb-dev/react';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <ThirdWebProvider desiredChainId={ChainId.Goerli}>
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
         <Router>
             <App />
         </Router>
-    </ThirdWebProvider>
+    </ThirdwebProvider>
 )
