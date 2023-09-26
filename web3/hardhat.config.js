f59910c0428d9e2fb05cb42fd5583066e2d61ac7
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-09-27 00:16:20 +0530

    chain id changed to 5

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,14 +1,12 @@
-require("@matterlabs/hardhat-zksync-solc");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.17",
+    version: '0.8.17',
     defaultNetwork: 'goerli',
     networks: {
       hardhat: {},
       goerli: {
-        url: 'https://rpc.ankr.com/eth_goerli',
+        url: 'https://goerli.infura.io/v3/${INFURA_PROJECT_ID}',
         accounts: [`0x${process.env.PRIVATE_KEY}`]
       }
     },
