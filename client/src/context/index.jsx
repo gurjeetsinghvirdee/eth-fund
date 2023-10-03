048015be4bc5b5c2675d0d905fd441dd556757bd
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-10-04 05:01:39 +0530

    created a new function as getUserCampaigns it will shows a campaigns created by user

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -47,6 +47,15 @@ export const StateContextProvider = ({ children }) => {
         return parsedCampaigns;
     }
 
+    const getUserCampaigns = async () => {
+        const allCampaigns = await getCampaigns();
+
+        const filteredCampaigns = allCampaigns.filter((campaign) => 
+        campaign.owner === address);
+
+        return filteredCampaigns;
+    }
+
     return (
         <StateContext.Provider
             value={{
@@ -55,6 +64,7 @@ export const StateContextProvider = ({ children }) => {
                 connect,
                 createCampaign: publishCampaign,
                 getCampaigns,
+                getUserCampaigns
             }}
         >
             {children}
