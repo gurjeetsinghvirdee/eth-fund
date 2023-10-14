60381e23a29b3f5c0f22e31b2404a759557f7f3e
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-10-15 04:23:52 +0530

    remove extra spacing

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -7,7 +7,7 @@ const FormField = ({ labelName, placeholder, inputType, isTextArea, value, handl
             <span className='font-epilogue font-medium text-[14px] 
             leading-[22px] text-[#808191] mb-[10px]'>{labelName}</span>
         )}
-        {isTextArea ? (
+       {isTextArea ? (
             <textarea 
                 required
                 value={value}
